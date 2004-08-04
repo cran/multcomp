82f9fd89e6c177567a7a043cfa30ddc51d5c1c05
Torsten Hothorn <Torsten.Hothorn@rzmail.uni-erlangen.de>
2004-08-04 14:38:48 +0000

    version 0.4-7

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
-# $Id: zzz.R,v 1.4 2003/06/23 13:35:22 hothorn Exp $
+# $Id: zzz.R,v 1.5 2004/08/04 08:38:22 hothorn Exp $
 
 .onLoad <- function(lib, pkg) {
-    if(!require(mvtnorm))
-        warning("Could not load package mvtnorm")
+    if(!require(mvtnorm, quietly = TRUE))
+        stop("Could not load package mvtnorm")
 }
